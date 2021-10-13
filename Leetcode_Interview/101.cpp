070e6090f7965e98893fbaa713d34c8838fd25f4
vasvi-sood <contactvasvisood@gmail.com>
2021-10-13 15:48:30 +0530

    is symmetric tre

---
+++ Leetcode_Interview/101.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+    bool isequal(TreeNode *root1, TreeNode *root2)
+    {
+        if (root1 == NULL && root2 == NULL)
+            return true;
+        if (root1 == NULL || root2 == NULL)
+            return false;
+        if (root1->val != root2->val)
+            return false;
+        bool ans = isequal(root1->left, root2->right);
+        ans = ans && isequal(root1->right, root2->left);
+        return ans;
+    }
+
+public:
+    bool isSymmetric(TreeNode *root)
+    {
+        if (root == NULL)
+            return true;
+        if (isequal(root->left, root->right))
+            return true;
+        return false;
+    }
+};
