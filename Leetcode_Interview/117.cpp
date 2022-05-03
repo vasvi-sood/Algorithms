dc07c6862787ac418eb6eb654ba9658ee8072861
vasvi-sood <contactvasvisood@gmail.com>
2022-05-03 10:33:43 +0530

    pointing next right pointer in each node

---
+++ Leetcode_Interview/117.cpp
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == NULL)
+            return root;
+
+        queue<Node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int len = q.size();
+            Node *prev = q.front();
+            q.pop();
+            if (prev->left != NULL)
+            {
+                q.push(prev->left);
+            }
+            if (prev->right != NULL)
+            {
+                q.push(prev->right);
+            }
+            for (int i = 1; i < len; i++)
+            {
+                Node *node = q.front();
+                q.pop();
+                prev->next = node;
+                prev = node;
+                if (prev->left != NULL)
+                {
+                    q.push(prev->left);
+                }
+                if (prev->right != NULL)
+                {
+                    q.push(prev->right);
+                }
+            }
+        }
+
+        return root;
+    }
+};
